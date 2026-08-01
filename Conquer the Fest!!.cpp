240ce2ee7b496a232339e0b704ee94ff511e0aab
VINAY <raminivinay777@gmail.com>
2026-08-01 05:48:48 +0530

    Add Conquer the Fest program to check conditions

---
+++ Conquer the Fest!!.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n, b;
+    cin >> n >> b;
+    if (n >= (b * 10)) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
