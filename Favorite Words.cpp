7c06f8ae5e6174c6fc14dd91056874f1984dd3e0
VINAY <raminivinay777@gmail.com>
2026-08-02 06:45:12 +0530

    Add Favorite Words program to check conditions

---
+++ Favorite Words.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char s[5];
+    cin >> s;
+    if (s[0] == 'c' || s[3] == 'f') {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
