22cca9a3040f3af9d223d24ab5e80291f646081a
VINAY <raminivinay777@gmail.com>
2026-08-03 20:27:25 +0530

    Add Extra Guests program to calculate costs

---
+++ Extra Guests.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    int extra = y - x;
+    cout << (x * 100) + (extra * 150) << endl;
+    return 0;
+}
