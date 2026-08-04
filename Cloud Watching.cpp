6dfd2dab55c4e0e08610446377e09e1e155e3688
VINAY <raminivinay777@gmail.com>
2026-08-04 06:13:01 +0530

    Add Cloud Watching program to determine weather

---
+++ Cloud Watching.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if ((a * 3) <= (b)) {
+        cout << "RAIN" << endl;
+    }
+    else {
+        cout << "DRY" << endl;
+    }
+    return 0;
+}
