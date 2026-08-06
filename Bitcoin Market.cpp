ab352abb80aa9ea26a0a3682dcabb7bc9b40dec6
VINAY <raminivinay777@gmail.com>
2026-08-06 06:27:35 +0530

    Add Bitcoin Market program to evaluate input

---
+++ Bitcoin Market.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int r;
+    cin >> r;
+    if (r <= 4) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
