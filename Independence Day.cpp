6fc5db1a1ef58c8f90374e37537055647e5b5ca8
VINAY <raminivinay777@gmail.com>
2026-08-07 06:31:20 +0530

    Add Independence Day program to calculate difference

---
+++ Independence Day.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x > 15) {
+        cout << -1;
+    }
+    else {
+        cout << 15 - x;
+    }
+    return 0;
+}
