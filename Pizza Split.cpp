7f02d2e035f217930a3b9255b37aacf2452a18df
VINAY <raminivinay777@gmail.com>
2026-08-08 07:41:57 +0530

    Add Pizza Split program to determine pizza slices

---
+++ Pizza Split.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << "1" << endl;
+    }
+    else {
+        cout << "2" << endl;
+    }
+    return 0;
+}
