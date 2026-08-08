de56ac02887c218cd5afbcec2d2104c00ee50364
VINAY <raminivinay777@gmail.com>
2026-08-08 07:45:15 +0530

    Add Vacation Clothes program to calculate outfits

---
+++ Vacation Clothes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n <= 7) {
+        cout << n << endl;
+    }
+    else {
+        cout << n - 3 << endl;
+    }
+    return 0;
+}
