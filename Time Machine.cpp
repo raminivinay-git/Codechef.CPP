cc0f5a3ca49c5be92bd0a28229395a090a2c7e69
VINAY <raminivinay777@gmail.com>
2026-08-09 06:23:27 +0530

    Add Time Machine program to check year

---
+++ Time Machine.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x >= 2025) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
