b1680375bc599d472e9a235e255b9263c84c48ae
VINAY <raminivinay777@gmail.com>
2026-08-12 05:20:56 +0530

    Implement string check in Fonta.cpp
    Check if the input string has specific characters at certain positions.

---
+++ Fonta.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char s[6];
+    cin >> s;
+    if (s[2] == 'n' && s[3] == 't' && s[4]) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
