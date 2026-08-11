f14fa4b40b90cb01b6c202a9dec993edbf46fc01
VINAY <raminivinay777@gmail.com>
2026-08-11 06:13:13 +0530

    Add program to check homework threshold

---
+++ Too Much Homework!.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if ((y * 10) + x >= 100) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
