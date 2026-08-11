65028fc574ba55f569d8cea58a1da8259a17c4de
VINAY <raminivinay777@gmail.com>
2026-08-12 05:25:35 +0530

    Implement Snaky Strings logic in main function

---
+++ Snaky Strings.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    char s[5];
+    cin >> s;
+    if (s[0] == 's' || s[3] == 's') {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
