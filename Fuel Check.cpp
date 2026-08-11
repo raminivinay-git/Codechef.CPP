01674faa90b59bb9fc361f3665144f06d3efeadc
VINAY <raminivinay777@gmail.com>
2026-08-11 06:08:06 +0530

    Add fuel check program to evaluate fuel efficiency

---
+++ Fuel Check.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    if ((x * y) >= 100) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
