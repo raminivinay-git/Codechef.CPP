fea0698acb04b8de30ce8456c6793bb3c8993ace
VINAY <raminivinay777@gmail.com>
2026-08-13 06:23:27 +0530

    Add Entertainments.cpp for entertainment cost calculation

---
+++ Entertainments.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (1000 > (n * 200)) {
+        cout << n * 200 << endl;
+    }
+    else {
+        cout << "1000" << endl;
+    }
+    return 0;
+}
