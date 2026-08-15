85016c8b093590f87d5ba4a87bdbadc09a8cdd5d
VINAY <raminivinay777@gmail.com>
2026-08-15 07:23:14 +0530

    Add Chef and Socks program to check conditions

---
+++ Chef and Socks.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, x, y;
+    cin >> a >> x >> y;
+    if ((x + y) >= a) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
