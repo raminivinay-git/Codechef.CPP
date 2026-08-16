0c194b2f9bef35b1b2188907e58e5b3f5325ef4b
VINAY <raminivinay777@gmail.com>
2026-08-16 05:47:54 +0530

    Add Move Grid.cpp for coordinate calculation

---
+++ Move Grid.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    a = a - c;
+    b = b - d;
+    cout << a << " " << b << endl;
+    return 0;
+}
