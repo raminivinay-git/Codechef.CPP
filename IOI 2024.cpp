084e9ce1f8faac751145d71830231a716e3c9df8
VINAY <raminivinay777@gmail.com>
2026-08-17 06:16:42 +0530

    Add basic input/output program for IOI 2024

---
+++ IOI 2024.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int x;
+    cin >> x;
+    if (x <= 8) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
