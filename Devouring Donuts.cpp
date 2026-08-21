9e3d84f757e88fe992162ee6cd4c7270a10eca33
VINAY <raminivinay777@gmail.com>
2026-08-21 11:07:58 +0530

    Add Devouring Donuts program to calculate calories

---
+++ Devouring Donuts.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+void calories(int x, int y) {
+    cout << x * y << endl;
+}
+int main() {
+    int x, y;
+    cin >> x >> y;
+    calories(x, y);
+    return 0;
+}
