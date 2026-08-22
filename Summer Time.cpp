d3763af8c291b2b022371525bc852a6be02f4199
VINAY <raminivinay777@gmail.com>
2026-08-22 13:07:13 +0530

    Add Summer Time program to check temperature

---
+++ Summer Time.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void lassi(int x) {
+    if (x > 35) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
+int main() {
+    int x;
+    cin >> x;
+    lassi(x);
+    return 0;
+}
