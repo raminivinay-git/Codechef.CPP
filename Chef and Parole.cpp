80bfa0070d4802a18f16f6d6d660b54a055973de
VINAY <raminivinay777@gmail.com>
2026-08-22 13:12:53 +0530

    Add Chef and Parole program implementation

---
+++ Chef and Parole.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void prison(int x) {
+    if (x >= 7) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
+int main() {
+    int x;
+    cin >> x;
+    prison(x);
+    return 0;
+}
