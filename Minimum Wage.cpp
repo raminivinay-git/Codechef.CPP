2651b9990b2c40d28eb998929d4575f09ebabe49
VINAY <raminivinay777@gmail.com>
2026-08-23 10:28:14 +0530

    Implement Minimum Wage check in Minimum Wage.cpp

---
+++ Minimum Wage.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void MinimumWage(int x) {
+    cout << (x > 11 ? "YES" : "NO") << endl;;
+}
+
+int main() {
+    int x;
+    cin >> x;
+    MinimumWage(x);
+    return 0;
+}
