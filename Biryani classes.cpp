574017777db15fc0b44f690dc30e2df906dbc6d4
VINAY <raminivinay777@gmail.com>
2026-08-26 05:57:47 +0530

    Add Biryani classes with multiplication functionality

---
+++ Biryani classes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Biryaniclasses(int x, int y) {
+    cout << x * y << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x, y;
+        cin >> x >> y;
+        Biryaniclasses(x, y);
+    }
+    return 0;
+}
