46bacc97514a425832d1e1c4278db762a80cae92
VINAY <raminivinay777@gmail.com>
2026-08-28 16:51:08 +0530

    Add function to calculate unattempted problems

---
+++ How many unattempted problems.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void HowManyUnattemptedProblems(int x, int y) {
+    cout << x - y << endl;
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    HowManyUnattemptedProblems(x, y);
+    return 0;
+}
