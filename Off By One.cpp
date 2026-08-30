38a4ece8cae1c703a2d834ea5ac3667ff96bef99
VINAY <raminivinay777@gmail.com>
2026-08-30 06:45:35 +0530

    Implement OffByOne function in Off By One.cpp

---
+++ Off By One.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void OffByOne(int a, int b) {
+    cout << a + b << "1" << endl;
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    OffByOne(a, b);
+    return 0;
+}
