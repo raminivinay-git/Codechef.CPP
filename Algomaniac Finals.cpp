3c10db9dd8b4241e746dfb153ab139ed6020a2eb
VINAY <raminivinay777@gmail.com>
2026-09-03 05:12:32 +0530

    Add Algomaniac Finals program

---
+++ Algomaniac Finals.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void AlgomaniacFinals(int x) {
+    if (x == 17) {
+        cout << "YAY" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    int x;
+    cin >> x;
+    AlgomaniacFinals(x);
+    return 0;
+}
