6405263746c36ada1220109448073d7c971dfec4
VINAY <raminivinay777@gmail.com>
2026-09-05 09:13:45 +0530

    Add Olympics 2024 program to calculate medal count

---
+++ Olympics 2024.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Olympics2024(int g, int s, int b) {
+    cout << 15 - (g + s + b);
+}
+
+int main() {
+    int g, s, b;
+    cin >> g >> s >> b;
+    Olympics2024(g, s, b);
+    return 0;
+}
