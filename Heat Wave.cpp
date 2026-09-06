02a5036cf91a8bc61f4375545cca02d855bba06d
VINAY <raminivinay777@gmail.com>
2026-09-06 08:32:32 +0530

    Add Heat Wave program to evaluate conditions

---
+++ Heat Wave.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void HeatWave(int x, int y) {
+    if (x >= y) {
+        cout << "NO" << endl;
+    }
+    else {
+        cout << "YES" << endl;
+    }
+}
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    HeatWave(x, y);
+    return 0;
+}
