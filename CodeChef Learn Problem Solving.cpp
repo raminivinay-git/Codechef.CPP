732e0ebd26736b61773db3be38c8cafd5dd243de
VINAY <raminivinay777@gmail.com>
2026-09-07 05:50:42 +0530

    Add CodeChef problem solving example

---
+++ CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int CodeChefLearnProblemSolving(int n) {
+    return n * 2;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << CodeChefLearnProblemSolving(n);
+    return 0;
+}
