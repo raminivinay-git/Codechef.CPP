67f2ec9506b87ba913e99fef8f076dfce4ade284
VINAY <raminivinay777@gmail.com>
2026-09-14 07:09:19 +0530

    Implement Ageing function to calculate age difference

---
+++ Ageing.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void Ageing(int x) {
+    cout << x - 10 << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int x;
+        cin >> x;
+        Ageing(x);
+    }
+    return 0;
+}
