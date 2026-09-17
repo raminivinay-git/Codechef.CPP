b082d6e5d742adcffb5e7c23c6d5ec69e4a01fa3
VINAY <raminivinay777@gmail.com>
2026-09-18 03:48:21 +0530

    Add Chairs Requirement program to calculate differences

---
+++ Chairs Requirement.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int X, Y;
+        cin >> X >> Y;
+
+        if (X > Y)
+            cout << X - Y << endl;
+        else
+            cout << 0 << endl;
+    }
+
+    return 0;
+}
