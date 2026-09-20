443dd454a20b904a7309a8aa2ac4cf48a4470f77
VINAY <raminivinay777@gmail.com>
2026-09-20 19:09:10 +0530

    Add program to calculate number of credits
    Implement a program to calculate total credits based on input values.

---
+++ Number of Credits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin >> T;
+
+    while (T--)
+    {
+        int X, Y, Z;
+        cin >> X >> Y >> Z;
+
+        cout << 4 * X + 2 * Y << endl;
+    }
+
+    return 0;
+}
