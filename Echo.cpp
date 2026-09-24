89bcffb8511dcce418c1f64280e04bdab1152456
VINAY <raminivinay777@gmail.com>
2026-09-24 08:58:02 +0530

    Implement echo program to check string pairs

---
+++ Echo.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    if (s[0] == s[2] && s[1] == s[3]) {
+        cout << "YES";
+    }
+    else {
+        cout << "NO";
+    }
+    return 0;
+}
